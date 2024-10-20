6ac078825f0dff972ddc52ed51ff363562fec227
iceYami <137309137+iceYami@users.noreply.github.com>
2024-10-20 09:51:57 +0200

    Create passgen.py

---
+++ passgen.py
@@ -0,0 +1,14 @@
+# Generador de Contraseñas Seguras de MG
+
+import random
+import string
+
+def gen_contrasena(longitud=12):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    contrasena = ''.join(random.choice(caracteres) for _ in range(longitud))
+    return contrasena
+
+print("¡Bienvenida al Generador de Contraseñas Seguras de MG!")
+
+contrasena = gen_contrasena()
+print("Aquí tienes tu contraseña, cuídala:", contrasena)
