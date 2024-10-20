dec40d62e855f727058d9ca680a0a704e65419cd
iceYami <137309137+iceYami@users.noreply.github.com>
2024-10-20 10:01:10 +0200

    Create retneg.py

---
+++ retneg.py
@@ -0,0 +1,3 @@
+#In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+def make_negative(number):
+    return -number if number > 0 else number
